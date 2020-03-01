d113cec324ac0ae5c9f85401e3b4276f2c96d565
leynier <leynier41@gmail.com>
2020-03-01 14:47:23 -0500

    Read data

--- main.R
+++ main.R
@@ -2,3 +2,9 @@ mode <- function(v) {
     uniqv <- unique(v)
     uniqv[which.max(tabulate(match(v, uniqv)))]
 }
+
+data <- read.csv(file = 'database.csv')
+
+age = data[, 5]
+overall = data[, 11]
+potential = data[, 12]
